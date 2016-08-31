d191f717870e13cdcfae8b9869f9e7ec4608985b
Geoffrey VILMONT <gvilmont@e1r1p5.42.fr>
2016-08-31 20:56:03 +0200

    maj menu

--- srcs/ft_menu.c
+++ srcs/ft_menu.c
@@ -6,7 +6,7 @@
 /*   By: gvilmont <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/31 19:34:45 by gvilmont          #+#    #+#             */
-/*   Updated: 2016/08/31 19:56:06 by gvilmont         ###   ########.fr       */
+/*   Updated: 2016/08/31 20:53:10 by gvilmont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,20 @@
 
 void	ft_menu(void)
 {
-	ft_putstr("\n\n      F  R  A  C  T  0  L     ");
-	ft_putstr("\n     #####################     ");
-	ft_putstr("\n             MENU             ");
-	ft_putstr("\n     #####################     ");
-	ft_putstr("\n     KEY            EFFECT");
-	ft_putstr("\n     ---------------------");
-	ft_putstr("\n     1 - 2         Switch");
-	ft_putstr("\n     3 - 4         Fractol");
-	ft_putstr("\n     ---------------------");
-	ft_putstr("\n     A      Original Color");
-	ft_putstr("\n     S          Blue Color");
-	ft_putstr("\n     ---------------------");
-	ft_putstr("\n     ESC              Exit\n\n");
+	ft_putstr("\n\n         F  R  A  C  T  0  L  ");
+	ft_putstr("\n     ###########################");
+	ft_putstr("\n                 MENU           ");
+	ft_putstr("\n     ###########################");
+	ft_putstr("\n     KEY                  EFFECT");
+	ft_putstr("\n     ---------------------------");
+	ft_putstr("\n     1 - 2                Switch");
+	ft_putstr("\n     3 - 4               Fractol");
+	ft_putstr("\n     ---------------------------");
+	ft_putstr("\n     A            Original Color");
+	ft_putstr("\n     S                Blue Color");
+	ft_putstr("\n     ---------------------------");
+	ft_putstr("\n     +        Increase Iteration");
+	ft_putstr("\n     -        Decrease Iteration");
+	ft_putstr("\n     ESC                    Exit\n\n");
 	return ;
 }
